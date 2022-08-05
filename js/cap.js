4fde2e16c9c43f84022795281a963d6ee557a21f
shkarubskiy <vshkarubskiy@gmail.com>
2022-08-05 11:35:48 +0600

    feat: cap page logic

--- js/cap.js
+++ js/cap.js
@@ -1,21 +1,36 @@
 const gameData = {
   turnOrder: null,
   keys: [],
+  classes: [],
 };
+const cards = document.querySelectorAll(".card");
 
 function getParams() {
   let url = window.location.search;
   url = url.replace("?", "");
   gameData.turnOrder = url[0];
-  let buffer = url.slice(1, url.length - 1);
-  for (let i = 0; i < buffer.length - 1; i += 2) {
-    if (`${buffer[i]}${buffer[i + 1]}` == "00")
-      gameData.keys.push("card-green");
-    if (`${buffer[i]}${buffer[i + 1]}` == "10") gameData.keys.push("card-red");
-    if (`${buffer[i]}${buffer[i + 1]}` == "01") gameData.keys.push("card-blue");
-    if (`${buffer[i]}${buffer[i + 1]}` == "11")
-      gameData.keys.push("card-black");
+  gameData.keys = url.slice(1, url.length);
+}
+
+function convertParams() {
+  for (let i = 0; i < gameData.keys.length - 1; i += 2) {
+    if (`${gameData.keys[i]}${gameData.keys[i + 1]}` == "00")
+      gameData.classes.push("card-green");
+    if (`${gameData.keys[i]}${gameData.keys[i + 1]}` == "10")
+      gameData.classes.push("card-red");
+    if (`${gameData.keys[i]}${gameData.keys[i + 1]}` == "01")
+      gameData.classes.push("card-blue");
+    if (`${gameData.keys[i]}${gameData.keys[i + 1]}` == "11")
+      gameData.classes.push("card-black");
   }
 }
 
+function assignCards() {
+  cards.forEach((card, index) => {
+    card.classList.add(gameData.classes[index]);
+  });
+}
+
 getParams();
+convertParams();
+assignCards();
